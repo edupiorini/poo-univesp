1915676d601174b32094a6396c0cbc2c870fb2cc
edupiorini <eduardopiorini@hotmail.com>
2020-10-09 06:58:35 -0300

    Cliente and Conta class are now separated

--- src/poounivesp/Conta.java
+++ src/poounivesp/Conta.java
@@ -1,52 +1,5 @@
 package poounivesp;
 
-class Cliente {
-    private String nome;
-    private String sobrenome;
-    private String CPF;
-    private String data_nasc;
-
-    public Cliente(String nome, String sobrenome, String cPF, String data_nasc) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.CPF = cPF;
-        this.data_nasc = data_nasc;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public String getCPF() {
-        return CPF;
-    }
-
-    public void setCPF(String cPF) {
-        CPF = cPF;
-    }
-
-    public String getData_nasc() {
-        return data_nasc;
-    }
-
-    public void setData_nasc(String data_nasc) {
-        this.data_nasc = data_nasc;
-    }
-
-}
-
 public class Conta extends Cliente {
 
     private String nroConta;
