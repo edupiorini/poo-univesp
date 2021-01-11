5d411d1b27f723d7c77df5fb29856a8aa05ec15d
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-01-11 04:43:18 -0300

    refactor o n Adress - PTbr to ENus

--- src/Models/Adress/Adress.java
+++ src/Models/Adress/Adress.java
@@ -1,68 +1,71 @@
 package Models.Adress;
 
 public class Adress {
-    private String rua, cidade, estado, país, cep;
+    private String street, city, state, country, zipCode;
     private int houseNumber;
 
-    public Adress(String rua, int houseNumber, String cidade, String estado, String país, String cep) {
+    public Adress(String street, int houseNumber, String city, String state, String country, String zipCode) {
         //throw exceptions
         if (houseNumber <= 0) {
             throw new IllegalArgumentException("Invalid house number");
             
         }
 
-        this.rua = rua;
+        this.street = street;
         this.houseNumber = houseNumber;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.país = país;
-        this.cep = cep;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.zipCode = zipCode;
     }
 
-    public void setHouseNumber(int houseNumber){
-        this.houseNumber = houseNumber;
+    public String getStreet() {
+      return street;
     }
+
+    public void setStreet(String street) {
+      this.street = street;
+    }
+
+    public String getCity() {
+      return city;
+    }
+
+    public void setCity(String city) {
+      this.city = city;
+    }
+
+    public String getState() {
+      return state;
+    }
+
+    public void setState(String state) {
+      this.state = state;
+    }
+
+    public String getCountry() {
+      return country;
+    }
+
+    public void setCountry(String country) {
+      this.country = country;
+    }
+
+    public String getZipCode() {
+      return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+      this.zipCode = zipCode;
+    }
+
     public int getHouseNumber() {
-        return houseNumber;
+      return houseNumber;
     }
 
-    public String getRua() {
-        return rua;
+    public void setHouseNumber(int houseNumber) {
+      this.houseNumber = houseNumber;
     }
 
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getPaís() {
-        return país;
-    }
-
-    public void setPaís(String país) {
-        this.país = país;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
+    
 }
